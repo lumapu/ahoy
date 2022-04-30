71c83ac1fb78a23787d947fea0e323f2ecc9a254
lumapu <lp@lufami.de>
2022-04-30 02:13:38 +0200

    * version 0.3.0 * added unit test

--- tools/esp8266/defines.h
+++ tools/esp8266/defines.h
@@ -24,8 +24,8 @@
 // VERSION
 //-------------------------------------
 #define VERSION_MAJOR       0
-#define VERSION_MINOR       2
-#define VERSION_PATCH       12
+#define VERSION_MINOR       3
+#define VERSION_PATCH       0
 
 
 //-------------------------------------
