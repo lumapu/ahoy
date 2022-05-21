93b3f02b246c7d4b2ea76f90d4821ae726f7fce4
Jan-Jonas Sämann <sprinterfreak@binary-kitchen.de>
2022-05-21 17:04:14 +0200

    Add pypackage retransmit last frame
    Mentioned in #30 by @stefan123t

--- tools/rpi/hoymiles/__init__.py
+++ tools/rpi/hoymiles/__init__.py
@@ -619,6 +619,8 @@ class InverterTransaction:
             self.time_rx = end_frame.time_rx
             tr_len = end_frame.seq - 0x80
         except StopIteration:
+            seq_last = max(frames, key=lambda frame:frame.seq).seq
+            self.__retransmit_frame(seq_last + 1)
             raise BufferError(f'Missing packet: Last packet {len(self.scratch)}')
 
         # Rebuild payload from unordered frames
