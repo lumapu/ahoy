375a1ca9553069b80a6928bb161b02041290e3e6
lumapu <lp@lufami.de>
2022-08-04 07:31:36 +0200

    increased version to 0.4.26

--- tools/esp8266/defines.h
+++ tools/esp8266/defines.h
@@ -21,7 +21,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       4
-#define VERSION_PATCH       25
+#define VERSION_PATCH       26
 
 
 //-------------------------------------
