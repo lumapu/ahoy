2f88fea22978ce2fd94cd3cd07be78ccd863719c
lumapu <lp@lufami.de>
2022-08-12 11:17:04 +0200

    fix #146 v0.5.8

--- tools/esp8266/defines.h
+++ tools/esp8266/defines.h
@@ -21,7 +21,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       5
-#define VERSION_PATCH       7
+#define VERSION_PATCH       8
 
 
 //-------------------------------------
