0aaed0908e98e61664286d429f3c08a39563e030
DanielR92 <DanielR92@users.noreply.github.com>
2022-09-27 19:36:26 +0200

    Clean commit, clean code & number

--- tools/esp8266/hmDefines.h
+++ tools/esp8266/hmDefines.h
@@ -18,7 +18,7 @@ union serial_u {
 
 // units
 enum {UNIT_V = 0, UNIT_A, UNIT_W,  UNIT_WH, UNIT_KWH, UNIT_HZ, UNIT_C, UNIT_PCT, UNIT_VAR, UNIT_NONE};
-const char* const units[] = {"V", "A", "W", "Wh", "kWh", "Hz", "°C", "%","var",""};
+const char* const units[] = {"V", "A", "W", "Wh", "kWh", "Hz", "°C", "%", "var", ""};
 
 // field types
 enum {FLD_UDC = 0, FLD_IDC, FLD_PDC, FLD_YD, FLD_YW, FLD_YT,
@@ -122,11 +122,11 @@ const byteAssign_t hm1chAssignment[] = {
     { FLD_UAC, UNIT_V,   CH0, 14, 2, 10   },
     { FLD_IAC, UNIT_A,   CH0, 22, 2, 100  },
     { FLD_PAC, UNIT_W,   CH0, 18, 2, 10   },
-    { FLD_Q, UNIT_VAR,  CH0, 20, 2, 10   },
+    { FLD_Q, UNIT_VAR,  CH0, 20, 2, 10    },
     { FLD_F,   UNIT_HZ,  CH0, 16, 2, 100  },
     { FLD_PF,  UNIT_NONE,CH0, 24, 2, 1000 },
     { FLD_T,   UNIT_C,   CH0, 26, 2, 10   },
-    { FLD_ALARM_MES_CNT,   UNIT_NONE,   CH0, 24, 2, 1 },
+    { FLD_ALARM_MES_CNT,   UNIT_NONE,   CH0, 28, 2, 1 },
     { FLD_YD,  UNIT_WH,  CH0, CALC_YD_CH0,  0, CMD_CALC },
     { FLD_YT,  UNIT_KWH, CH0, CALC_YT_CH0,  0, CMD_CALC },
     { FLD_PDC, UNIT_W,   CH0, CALC_PDC_CH0, 0, CMD_CALC },
@@ -156,7 +156,7 @@ const byteAssign_t hm2chAssignment[] = {
     { FLD_UAC, UNIT_V,   CH0, 26, 2, 10   },
     { FLD_IAC, UNIT_A,   CH0, 34, 2, 100  },
     { FLD_PAC, UNIT_W,   CH0, 30, 2, 10   },
-    { FLD_Q, UNIT_VAR,  CH0, 32, 2, 10   },
+    { FLD_Q, UNIT_VAR,  CH0, 32, 2, 10    },
     { FLD_F,   UNIT_HZ,  CH0, 28, 2, 100  },
     { FLD_PF,  UNIT_NONE,CH0, 36, 2, 1000 },
     { FLD_T,   UNIT_C,   CH0, 38, 2, 10   },
@@ -204,9 +204,9 @@ const byteAssign_t hm4chAssignment[] = {
     { FLD_UAC, UNIT_V,   CH0, 46, 2, 10   },
     { FLD_IAC, UNIT_A,   CH0, 54, 2, 100  },
     { FLD_PAC, UNIT_W,   CH0, 50, 2, 10   },
-    { FLD_Q, UNIT_VAR,  CH0, 52, 2, 10   },
+    { FLD_Q, UNIT_VAR,  CH0, 52, 2, 10    },
     { FLD_F,   UNIT_HZ,  CH0, 48, 2, 100  },
-    { FLD_PF, UNIT_PCT, CH0, 56, 2, 10   },
+    { FLD_PF, UNIT_PCT, CH0, 56, 2, 1000  },
     { FLD_T,   UNIT_C,   CH0, 58, 2, 10   },
     { FLD_ALARM_MES_CNT,  UNIT_NONE,   CH0, 60, 2, 1   },
     { FLD_YD,  UNIT_WH,  CH0, CALC_YD_CH0,  0, CMD_CALC },
