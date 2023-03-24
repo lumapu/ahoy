d61b707a76b123d9ecbd4689d77a44fe8d126472
lumapu <lp@lufami.de>
2023-03-24 18:05:33 +0100

    switched config of cmt - RX still working

--- src/hms/hmsPayload.h
+++ src/hms/hmsPayload.h
@@ -149,7 +149,7 @@ class HmsPayload {
                 uint8_t cmd = iv->getQueuedCmd();
                 DPRINT(DBG_INFO, F("(#"));
                 DBGPRINT(String(iv->id));
-                DBGPRINT(F(") prepareDevInformCmd")); // + String(cmd, HEX));
+                DBGPRINTLN(F(") prepareDevInformCmd")); // + String(cmd, HEX));
                 mRadio->prepareDevInformCmd(&iv->radioId.u64, cmd, mPayload[iv->id].ts, iv->alarmMesIndex, false);
                 mPayload[iv->id].txCmd = cmd;
             }
