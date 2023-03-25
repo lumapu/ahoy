cccbe63f2b85777a1cf5fdb3dc60e58591c5ffa8
Markus Krause <Krause.Markus@gmx.com>
2023-03-25 20:46:25 +0100

    fix config override example
    (add spi pins, remove wrong RF24 in there which was not correct even before)

--- src/config/config_override_example.h
+++ src/config/config_override_example.h
@@ -18,13 +18,18 @@
 #define FB_WIFI_PWD     "MY_WIFI_KEY"
 
 // ESP32 default pinout
-#undef DEF_RF24_CS_PIN
-#define DEF_RF24_CS_PIN         5
-#undef DEF_RF24_CE_PIN
-#define DEF_RF24_CE_PIN         4
-#undef DEF_RF24_IRQ_PIN
-#define DEF_RF24_IRQ_PIN        16
-
+#undef DEF_CS_PIN
+#define DEF_CS_PIN 37
+#undef DEF_CE_PIN
+#define DEF_CE_PIN 38
+#undef DEF_IRQ_PIN
+#define DEF_IRQ_PIN 47
+#undef DEF_MISO_PIN
+#define DEF_MISO_PIN 48
+#undef DEF_MOSI_PIN
+#define DEF_MOSI_PIN 35
+#undef DEF_SCLK_PIN
+#define DEF_SCLK_PIN 36
 
 // Offset for midnight Ticker Example: 1 second before midnight (local time)
 #undef MIDNIGHTTICKER_OFFSET
