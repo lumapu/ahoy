081adeb140a4b74220990b16b935d1fc305fc61b
lumapu <lp@lufami.de>
2023-03-26 01:03:59 +0100

    0.5.107 * fix: show save message * fix: removed serial newline for `enqueueCmd`

--- src/defines.h
+++ src/defines.h
@@ -13,7 +13,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       5
-#define VERSION_PATCH       106
+#define VERSION_PATCH       107
 
 //-------------------------------------
 typedef struct {
