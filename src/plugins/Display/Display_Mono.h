81dfff15ca52f680fa3e70163b5abb7bee309064
lumapu <lp@lufami.de>
2023-09-04 21:36:02 +0200

    0.7.46 * fix compile

--- src/plugins/Display/Display_Mono.h
+++ src/plugins/Display/Display_Mono.h
@@ -23,7 +23,7 @@ class DisplayMono {
 
       virtual void init(uint8_t type, uint8_t rot, uint8_t cs, uint8_t dc, uint8_t reset, uint8_t clock, uint8_t data, uint32_t* utcTs, const char* version) = 0;
       virtual void config(bool enPowerSafe, bool enScreenSaver, uint8_t lum) = 0;
-      virtual void loop(void) = 0;
+      virtual void loop(uint8_t lum) = 0;
       virtual void disp(float totalPower, float totalYieldDay, float totalYieldTotal, uint8_t isprod) = 0;
 
    protected:
