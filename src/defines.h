024445b472005fbe7e71d847abb675053f1fd554
lumapu <lp@lufami.de>
2024-02-17 00:26:24 +0100

    0.8.83 release * updated workflows

--- src/defines.h
+++ src/defines.h
@@ -13,7 +13,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       8
-#define VERSION_PATCH       82
+#define VERSION_PATCH       83
 
 //-------------------------------------
 typedef struct {
