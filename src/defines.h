104033073ec36b0a8688d76985c1d241f8a6f1b5
lumapu <lp@lufami.de>
2024-03-24 16:59:08 +0100

    0.8.98 * new network routines

--- src/defines.h
+++ src/defines.h
@@ -13,7 +13,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       8
-#define VERSION_PATCH       97
+#define VERSION_PATCH       98
 
 //-------------------------------------
 typedef struct {
