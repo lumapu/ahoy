238497cb05b37bc927c51e1c820db3f6c87ebe78
Patrick Amrhein <patrick.amrhein@t-online.de>
2024-07-11 18:46:50 +0200

    0.8.1250004

--- src/defines.h
+++ src/defines.h
@@ -13,7 +13,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       8
-#define VERSION_PATCH       1250001
+#define VERSION_PATCH       1250004
 //-------------------------------------
 typedef struct {
     uint8_t ch;
