351f2b2c1c467c649a7731b9b8d550941d6f8d4b
lumapu <lp@lufami.de>
2024-08-16 16:35:06 +0200

    0.8.140 release

--- src/defines.h
+++ src/defines.h
@@ -13,7 +13,7 @@
 //-------------------------------------
 #define VERSION_MAJOR       0
 #define VERSION_MINOR       8
-#define VERSION_PATCH       139
+#define VERSION_PATCH       140
 //-------------------------------------
 typedef struct {
     uint8_t ch;
